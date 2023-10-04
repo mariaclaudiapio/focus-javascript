14e4f8ecb364176454964d76550caf1526724717
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-10-03 22:30:43 -0300

    changing images

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const image = document.querySelector('.app__image');
 
 focoBt.addEventListener('click', () => {
   html.setAttribute('data-contexto', 'foco');
+  image.setAttribute('src', '/imagens/foco.png');
 });
 
 curtoBt.addEventListener('click', () => {
   html.setAttribute('data-contexto', 'descanso-curto');
+  image.setAttribute('src', '/imagens/descanso-curto.png');
 });
 
 longoBt.addEventListener('click', () => {
   html.setAttribute('data-contexto', 'descanso-longo');
+  image.setAttribute('src', '/imagens/descanso-longo.png');
 });
